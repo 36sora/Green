80e284945bec44159ff5ebce9c78c0c934b8dba8
Yasu <zoo0165@gmail.com>
2012-04-18 17:49:33 +0900

    AOJ8問re

--- 10007.c
+++ 10007.c
@@ -3,16 +3,19 @@
 int main(void)
 {
 	int a,b;
+	while(1)
+	{
 
-	scanf("%d %d",&a,&b);
-	if (a < b)
-	{
-		printf("%d %d\n",a,b);
-	}else
-	{
-		printf("%d %d\n",b,a);
+		scanf("%d %d",&a,&b);
+		if ((a == 0) && (b == 0)) return 0;
+		if (a < b)
+		{
+			printf("%d %d\n",a,b);
+		}else
+		{
+			printf("%d %d\n",b,a);
+		}
+		return 0;
 	}
-
-	return 0;
 }
 
